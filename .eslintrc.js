36dd2f5153e346f825d8c6b20e9c10dbd81c10fc
刘伟 <841489295@qq.com>
2020-03-01 21:16:53 +0800

    修改完善hook请求方式，使vscode支持新语法

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: '@react-native-community', // 定义文件继承的子规范
+  parser: '@typescript-eslint/parser', // 定义ESLint的解析器
+  plugins: ['@typescript-eslint'], // 定义了该eslint文件所依赖的插件
 };
--- src/screens/FeedListScreen/FeedListScreen.tsx
+++ src/screens/FeedListScreen/FeedListScreen.tsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect, useRef} from 'react';
-import {View, SafeAreaView, Text, FlatList} from 'react-native';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
+import {SafeAreaView, FlatList} from 'react-native';
 import {Report, ScreensParamList} from 'types/types';
 import {selectUser} from 'reduxState/selectors';
 import {useSelector} from 'react-redux';
@@ -23,45 +23,48 @@ export default function FeedListScreen({}: Props) {
   const isFetching = useRef(false);
   const isFocused = useIsFocused();
   const route = useRoute<FeedListScreenRouteProp>();
-    const showMe = route.params?.showMe ?? false; // !双问号语法暂不支持，修改tsconfig无效，待解决
+  const showMe = route.params?.showMe ?? false;
+
+  const getDataList = useCallback(
+    async function getDataList(isRefresh?: boolean) {
+      if (isFetching.current) {
+        return;
+      }
+      if (!isRefresh && isEndReached.current) {
+        return;
+      }
+      isFetching.current = true;
+      setLoading(isRefresh ? 'refresh' : 'more');
+      const {data} = await get(
+        `/feef?${qs.stringify({
+          offset: isRefresh ? 0 : offset,
+          limit,
+          userId: showMe ? user.id : undefined,
+        })}`,
+      );
+      setLoading(null);
+      if (data && data.rows) {
+        let currentCount;
+        if (isRefresh) {
+          currentCount = data.rows.length;
+          setDataList(data.rows);
+        } else {
+          currentCount = data.rows.length + dataList.length;
+          setDataList(dataList.concat(data.rows));
+        }
+        setOffset(currentCount);
+        isEndReached.current = currentCount >= data.count;
+      }
+      isFetching.current = false;
+    },
+    [dataList, offset, showMe, user.id],
+  );
 
   useEffect(() => {
     if (isFocused) {
       getDataList(true);
     }
-  }, [isFocused]);
-
-  async function getDataList(isRefresh?: boolean) {
-    if (isFetching.current) {
-      return;
-    }
-    if (!isRefresh && isEndReached.current) {
-      return;
-    }
-    isFetching.current = true;
-    setLoading(isRefresh ? 'refresh' : 'more');
-    const {ok, msg, data} = await get(
-      `/feef?${qs.stringify({
-        offset: isRefresh ? 0 : offset,
-        limit,
-        userId: showMe ? user.id : undefined,
-      })}`,
-    );
-    setLoading(null);
-    if (data && data.rows) {
-      let currentCount;
-      if (isRefresh) {
-        currentCount = data.rows.length;
-        setDataList(data.rows);
-      } else {
-        currentCount = data.rows.length + dataList.length;
-        setDataList(dataList.concat(data.rows));
-      }
-      setOffset(currentCount);
-      isEndReached.current = currentCount >= data.count;
-    }
-    isFetching.current = false;
-  }
+  }, [getDataList, isFocused]);
 
   return (
     <SafeAreaView>
